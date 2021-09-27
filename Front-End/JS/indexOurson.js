9d9563b5407abcd3f97c83e6e82912e49dae8cb4
RGaetanGranstek <xynfsu@aol.com>
2021-09-27 14:09:37 +0200

    Corrections, ajout de liens nav, préparation page produit

--- Front-End/JS/indexOurson.js
+++ Front-End/JS/indexOurson.js
@@ -29,24 +29,31 @@ class ficheOurson {
     this.quantity[0] = quantity;
   }
 }
-
-console.log(ficheOurson);
+// console.log(ficheOurson);
 
 // fonction pour créé des cartes de mes produits en page d'accueil
 function addCarte(value) {
   //boucle pour chaque iteration d'un produit
   for (ficheOurson of value) {
-    //recupère l'élément Ourson dans le HTML
+    //recupère l'élément Ourson dans le HTML pour les cartes
     const carte = document.getElementById("Ourson");
     carte.innerHTML += `
     <div class="col-sm-6 p-3 mb-5 card">
-      <a href="./frontend/Pages/produit.html?_id=${ficheOurson._id}"></a>
-      <img src="${ficheOurson.imageUrl}" class="img-fluid img-thumbnail p-1" alt="${ficheOurson.name}">
+      <a href="./frontend/Pages/produit.html?_id=${ficheOurson._id}">
+        <img src="${ficheOurson.imageUrl}" class="img-fluid img-thumbnail p-1" alt="${ficheOurson.name}">
+      </a>
+      <div class="row card-body">
+        <h2 class="col-6 card-title">${ficheOurson.name}</h3>
+        <h3 class="col-6 text-right">${ficheOurson.price} euros</h3>
+      </div>
       <div class="card-body">
-          <h2 class="card-title">${ficheOurson.name}</h3>
-          <p class="card-text text-truncate">${ficheOurson.description}</p>
+        <p class="card-text text-truncate">${ficheOurson.description}</p>
       </div>
       <a href="./frontend/Pages/produit.html?_id=${ficheOurson._id}" class="btn btn-secondary">Acheter ce produit</a>
     </div>`;
+    //recupère l'élément linkOurson dans le nav pour créer des liens vers les produits
+    const linkOurson = document.getElementById("linkOurson");
+    linkOurson.innerHTML += `
+    <a class="dropdown-item text-white" href="./frontend/Pages/produit.html?_id=${ficheOurson._id}">Peluche => ${ficheOurson.name}</a>`;
   }
 }
