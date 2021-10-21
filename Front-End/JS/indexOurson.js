fd9a3a25115395ead8f821ff80042194d0de48b4
RGaetanGranstek <xynfsu@aol.com>
2021-10-21 11:01:28 +0200

    Modification suite conseil mentor

--- Front-End/JS/indexOurson.js
+++ Front-End/JS/indexOurson.js
@@ -8,7 +8,7 @@ function addCarte(value) {
     const carte = document.getElementById("Ourson");
     carte.innerHTML += `
     <div class="col-sm-6 p-3 mb-5 card">
-      <a href="./Front-End/Pages/produit.html?_id=${FicheOurson._id}" class="text-center">
+      <a href="../Pages/produit.html?_id=${FicheOurson._id}" class="text-center">
         <img src="${FicheOurson.imageUrl}" class="img-fluid img-thumbnail p-1" alt="image représentant ${FicheOurson.name}">
       </a>
       <div class="row card-body justify-content-around">
@@ -17,7 +17,7 @@ function addCarte(value) {
       <div class="card-body">
         <p class="card-text text-truncate">${FicheOurson.description}</p>
       </div>
-      <a href="./Front-End/Pages/produit.html?_id=${FicheOurson._id}" class="btn btn-outline-success">Acheter ce produit</a>
+      <a href="../Pages/produit.html?_id=${FicheOurson._id}" class="btn btn-outline-success">Acheter ce produit</a>
     </div>`;
     //recupère l'élément carouselOurson dans le header pour créer le carousel
     const carouselOurson = document.getElementById("carouselOurson");
